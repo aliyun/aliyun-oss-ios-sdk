b8449f3678b9fa14f361cfdfb39ae65339f88191
lingkun.lk <lingkun.lk@alibaba-inc.com>
2016-01-12 11:23:15 +0800

    modify the packing path problem.

--- buildFramework.sh
+++ buildFramework.sh
@@ -26,9 +26,9 @@ then
     rm -rf "${INSTALL_DIR}"
 fi
 
-mkdir -p "${INSTALL_DIR}"
+#mkdir -p "${INSTALL_DIR}"
 
-cp -R "${DEVICE_DIR}/" "${INSTALL_DIR}/.."
+cp -R "${DEVICE_DIR}" "${INSTALL_DIR}"
 
 # Uses the Lipo Tool to merge both binary files (i386 + armv6/armv7) into one Universal final product.
 lipo -create "${DEVICE_DIR}/${FMK_NAME}" "${SIMULATOR_DIR}/${FMK_NAME}" -output "${INSTALL_DIR}/${FMK_NAME}"
