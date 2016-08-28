490b0ad70f35e46b08209ee1a25c7e880d5d837f
周卓 <yecan.xyc@alibaba-inc.com>
2016-08-28 21:32:41 +0800

    release 2.5.2

--- AliyunOSSiOS.podspec
+++ AliyunOSSiOS.podspec
@@ -2,7 +2,7 @@ Pod::Spec.new do |s|
 
   s.name         = "AliyunOSSiOS"
 
-  s.version      = "2.5.1"
+  s.version      = "2.5.2"
 
   s.summary      = "An iOS SDK for Aliyun Object Storage Service"
 
--- AliyunOSSiOS/OSSDefine.h
+++ AliyunOSSiOS/OSSDefine.h
@@ -12,7 +12,7 @@
 #define OSSDefine_h
 
 #define OSSUAPrefix                             @"aliyun-sdk-ios"
-#define OSSSDKVersion                           @"2.5.1"
+#define OSSSDKVersion                           @"2.5.2"
 
 #define OSSListBucketResultXMLTOKEN             @"ListBucketResult"
 #define OSSNameXMLTOKEN                         @"Name"
