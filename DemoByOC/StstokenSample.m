48f1b87d8465e6e86b017033fbcfa0ef44f26cb1
jingdan <haibing.whb@alibaba-inc.com>
2017-09-12 15:29:22 +0800

    fixed ip

--- DemoByOC/StstokenSample.m
+++ DemoByOC/StstokenSample.m
@@ -13,7 +13,7 @@
 
 @implementation StstokenSample
 
-const NSString* url = @"http://xx.xx.xx.xx:12555/sts/getsts";//本地服务地址
+const NSString* url = @"http://0.0.0.0:12555/sts/getsts";//本地服务地址
     //如何启动本地服务可参加python 目录下httpserver.py中注释说明。xx.xx.xx.xx 为本机ip地址。xxx
 
 
