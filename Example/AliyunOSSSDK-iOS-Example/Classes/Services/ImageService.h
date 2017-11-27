6fbcc3565ce8d137990fa5067fcaaffa16a3c7ad
jingdan <haibing.whb@alibaba-inc.com>
2017-11-27 10:12:25 +0800

    change author

--- Example/AliyunOSSSDK-iOS-Example/Classes/Services/ImageService.h
+++ Example/AliyunOSSSDK-iOS-Example/Classes/Services/ImageService.h
@@ -2,7 +2,7 @@
 //  ImageService.h
 //  OssIOSDemo
 //  使用图片服务处理图片
-//  Created by 凌琨 on 15/12/15.
+//  Created by jingdan on 17/11/23.
 //  Copyright © 2015年 Ali. All rights reserved.
 //
 #ifndef ImageService_h
--- Example/AliyunOSSSDK-iOS-Example/Classes/Services/OssService.h
+++ Example/AliyunOSSSDK-iOS-Example/Classes/Services/OssService.h
@@ -1,8 +1,7 @@
 //
 //  OssService.h
 //  OssIOSDemo
-//
-//  Created by 凌琨 on 15/12/15.
+//  Created by jingdan on 17/11/23.
 //  Copyright © 2015年 Ali. All rights reserved.
 //
 
