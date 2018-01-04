bcaaf75ac8567e1c1317b9e6cd096c834e4ef7ab
runsheng <sdlcdhch@gmail.com>
2018-01-04 16:46:15 +0800

    测试信息还原

--- OSSSwiftDemo/OSSSwiftDemo/OSSSwiftGlobalDefines.swift
+++ OSSSwiftDemo/OSSSwiftDemo/OSSSwiftGlobalDefines.swift
@@ -8,15 +8,15 @@
 
 import Foundation
 
-let OSS_ACCESSKEY_ID: String = "LTAILXJfYS0JFkIf"
-let OSS_SECRETKEY_ID: String = "EJORuCccvpLrLDTvZWh5OtXVIvWk0T"
-let OSS_BUCKET_PUBLIC: String = "huaixu-shenzhen"
-let OSS_BUCKET_PRIVATE: String = "huaixu-private"
-let OSS_ENDPOINT: String = "http://oss-cn-shenzhen.aliyuncs.com"
-let OSS_MULTIPART_UPLOADKEY: String = "multipart"
-let OSS_RESUMABLE_UPLOADKEY: String = "resumable"
-let OSS_CALLBACK_URL: String = "http://oss-demo.aliyuncs.com:23450"
-let OSS_CNAME_URL: String = "https://hs.chenhongyu.cn"
-let OSS_STSTOKEN_URL: String = "http://30.40.38.20:3015/sts/getsts"
-let OSS_IMAGE_KEY: String = "hasky.jpeg"
+let OSS_ACCESSKEY_ID: String = "OSS_ACCESSKEY_ID"
+let OSS_SECRETKEY_ID: String = "OSS_SECRETKEY_ID"
+let OSS_BUCKET_PUBLIC: String = "OSS_BUCKET_PUBLIC"
+let OSS_BUCKET_PRIVATE: String = "OSS_BUCKET_PRIVATE"
+let OSS_ENDPOINT: String = "http://region.aliyuncs.com"
+let OSS_MULTIPART_UPLOADKEY: String = "OSS_MULTIPART_UPLOADKEY"
+let OSS_RESUMABLE_UPLOADKEY: String = "OSS_RESUMABLE_UPLOADKEY"
+let OSS_CALLBACK_URL: String = "OSS_CALLBACK_URL"
+let OSS_CNAME_URL: String = "OSS_CNAME_URL"
+let OSS_STSTOKEN_URL: String = "OSS_STSTOKEN_URL"
+let OSS_IMAGE_KEY: String = "OSS_IMAGE_KEY"
 
