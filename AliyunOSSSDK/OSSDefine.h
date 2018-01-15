46e4f7b5bc3d5655730fe650995cc7db224c7d1c
1019272778 <1019272778@qq.com>
2018-01-15 14:57:32 +0800

    Log: 2.8.1 release fix oom issue

--- AliyunOSSSDK/OSSDefine.h
+++ AliyunOSSSDK/OSSDefine.h
@@ -16,7 +16,7 @@
 #elif TARGET_OS_OSX
 #define OSSUAPrefix                             @"aliyun-sdk-mac"
 #endif
-#define OSSSDKVersion                           @"2.8.0"
+#define OSSSDKVersion                           @"2.8.1"
 
 #define OSSListBucketResultXMLTOKEN             @"ListBucketResult"
 #define OSSNameXMLTOKEN                         @"Name"
--- AliyunOSSiOS.podspec
+++ AliyunOSSiOS.podspec
@@ -2,7 +2,7 @@ Pod::Spec.new do |s|
 
   s.name         = "AliyunOSSiOS"
 
-  s.version      = "2.8.0"
+  s.version      = "2.8.1"
 
   s.summary      = "An iOS SDK for Aliyun Object Storage Service"
 
