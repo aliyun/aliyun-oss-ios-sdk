2718a2e5266a018e72c4fc92b1ec9eb9a86cb0bf
dhc <798277342@qq.com>
2018-05-16 20:01:30 +0800

    Log: upgrade version to 2.10.4

--- AliyunOSSSDK/OSSDefine.h
+++ AliyunOSSSDK/OSSDefine.h
@@ -16,7 +16,7 @@
 #elif TARGET_OS_OSX
 #define OSSUAPrefix                             @"aliyun-sdk-mac"
 #endif
-#define OSSSDKVersion                           @"2.10.3"
+#define OSSSDKVersion                           @"2.10.4"
 
 #define OSSListBucketResultXMLTOKEN             @"ListBucketResult"
 #define OSSNameXMLTOKEN                         @"Name"
