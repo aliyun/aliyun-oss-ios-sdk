e5866500694e372b053726a155e1fe9d4aad4960
润生 <sdlcdhch@gmail.com>
2018-07-10 17:24:10 +0800

    add Restful API which named getBucketInfo

---
+++ AliyunOSSSDK/OSSGetBucketInfoRequest.m
@@ -0,0 +1,13 @@
+//
+//  OSSGetBucketInfoRequest.m
+//  AliyunOSSSDK
+//
+//  Created by huaixu on 2018/7/10.
+//  Copyright © 2018年 aliyun. All rights reserved.
+//
+
+#import "OSSGetBucketInfoRequest.h"
+
+@implementation OSSGetBucketInfoRequest
+
+@end
