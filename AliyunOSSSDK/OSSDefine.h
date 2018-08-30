6a87dc7a913727ce9872b58980ccbde8bcdc1e4a
润生 <sdlcdhch@gmail.com>
2018-08-30 15:14:41 +0800

    release 2.10.6

--- AliyunOSSSDK/OSSDefine.h
+++ AliyunOSSSDK/OSSDefine.h
@@ -16,7 +16,7 @@
 #elif TARGET_OS_OSX
 #define OSSUAPrefix                             @"aliyun-sdk-mac"
 #endif
-#define OSSSDKVersion                           @"2.10.5"
+#define OSSSDKVersion                           @"2.10.6"
 
 #define OSSListBucketResultXMLTOKEN             @"ListBucketResult"
 #define OSSNameXMLTOKEN                         @"Name"
