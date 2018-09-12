f1de62986c76ca345a200d14c929464e5fee275a
润生 <sdlcdhch@gmail.com>
2018-09-12 17:28:57 +0800

    release 2.10.7 info

--- AliyunOSSSDK/OSSDefine.h
+++ AliyunOSSSDK/OSSDefine.h
@@ -16,7 +16,7 @@
 #elif TARGET_OS_OSX
 #define OSSUAPrefix                             @"aliyun-sdk-mac"
 #endif
-#define OSSSDKVersion                           @"2.10.6"
+#define OSSSDKVersion                           @"2.10.7"
 
 #define OSSListBucketResultXMLTOKEN             @"ListBucketResult"
 #define OSSNameXMLTOKEN                         @"Name"
--- AliyunOSSiOS.podspec
+++ AliyunOSSiOS.podspec
@@ -2,7 +2,7 @@ Pod::Spec.new do |s|
 
   s.name         = "AliyunOSSiOS"
 
-  s.version      = "2.10.6"
+  s.version      = "2.10.7"
 
   s.summary      = "An iOS SDK for Aliyun Object Storage Service"
 
