a72d9e2bc6dd905e43125ac9eebf96cf00f32924
剑子 <jianzi@IT-FVFW24L2J1WL.local>
2019-12-23 10:17:52 +0800

    修改版本信息

--- AliyunOSSSDK/OSSDefine.h
+++ AliyunOSSSDK/OSSDefine.h
@@ -16,7 +16,7 @@
 #elif TARGET_OS_OSX
 #define OSSUAPrefix                             @"aliyun-sdk-mac"
 #endif
-#define OSSSDKVersion                           @"2.10.7"
+#define OSSSDKVersion                           @"2.10.8"
 
 #define OSSListBucketResultXMLTOKEN             @"ListBucketResult"
 #define OSSNameXMLTOKEN                         @"Name"
