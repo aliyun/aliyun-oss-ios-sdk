de490e637ed3255390a3e54d6c9bb6a94c5f4715
ws <ws1350@hotmail.com>
2023-05-29 18:31:14 +0800

    Modify the information of version 2.10.17

--- AliyunOSSSDK/OSSDefine.h
+++ AliyunOSSSDK/OSSDefine.h
@@ -16,7 +16,7 @@
 #elif TARGET_OS_OSX
 #define OSSUAPrefix                             @"aliyun-sdk-mac"
 #endif
-#define OSSSDKVersion                           @"2.10.16"
+#define OSSSDKVersion                           @"2.10.17"
 
 #define OSSListBucketResultXMLTOKEN             @"ListBucketResult"
 #define OSSNameXMLTOKEN                         @"Name"
